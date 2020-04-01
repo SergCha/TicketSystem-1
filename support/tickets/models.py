04a64b5b7a856cde0d209e4197b133c9c34a5d08
Alex Korablev <akorablev@yandex-team.ru>
2020-04-01 16:37:06 +0500

    models improvements

--- support/tickets/models.py
+++ support/tickets/models.py
@@ -1,5 +1,6 @@
 import uuid as uuid
 
+from annoying.fields import AutoOneToOneField
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -8,14 +9,7 @@ class SupportOfficer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
-class Ticket(models.Model):
-    uuid = models.UUIDField(
-        primary_key=True, default=uuid.uuid4, editable=False)
-    subject = models.TextField()
-    body = models.TextField()
-    reporter_email = models.EmailField()
-    reporter_name = models.TextField()
-
+class TicketStatus(models.Model):
     STATE_CHOICES = [
         ('NE', 'New'),
         ('PR', 'Processed'),
@@ -29,17 +23,30 @@ class Ticket(models.Model):
         default='NE',
     )
 
+
+class Ticket(models.Model):
+    uuid = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False)
+    subject = models.TextField()
+    body = models.TextField()
+    reporter_email = models.EmailField()
+    reporter_name = models.TextField()
+
+    state = AutoOneToOneField(TicketStatus, on_delete=models.CASCADE)
+
     created_at = models.DateTimeField(auto_now_add=True)
     changed_at = models.DateTimeField(auto_now=True)
-    changed_by = models.ForeignKey('SupportOfficer', on_delete=None, default=None, null=True, blank=True)
+    changed_by = models.ForeignKey('SupportOfficer', on_delete=models.DO_NOTHING, default=None, null=True, blank=True)
 
 
 class Reactions(models.Model):
     ticket = models.ForeignKey('Ticket', on_delete=models.CASCADE)
+    state = AutoOneToOneField(TicketStatus, on_delete=models.CASCADE)
     body = models.TextField()
+    officer = models.ForeignKey('SupportOfficer', default=None, null=True, on_delete=models.DO_NOTHING)
 
     created_at = models.DateTimeField(auto_now_add=True)
-    changed_at = models.DateTimeField(auto_now=True)
-    changed_by = models.ForeignKey('SupportOfficer', on_delete=None, default=None, null=True)
+
+    parent = models.ForeignKey('Reactions', on_delete=models.DO_NOTHING, null=True)
 
 
