42f2c839b5ab87102db4a71636e70b63d9fd8e8e
SergCha <sergejgen74irbit@mail.ru>
2020-04-08 18:32:21 +0500

    поправил

--- support/tickets/urls.py
+++ support/tickets/urls.py
@@ -11,7 +11,6 @@ from . import views
 urlpatterns = [
     path('<uuid:uuid>/', views.get_ticket_by_uuid),
     path('add/', views.post_new_ticket),
-]
-
     path('', views.home, name='home'),
     path('', views.check, name='check'),
+]
